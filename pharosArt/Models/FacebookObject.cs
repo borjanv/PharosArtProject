92bb0baff6f669349ee032c052c222a89fc94f3c
borjanv <vasovski@me.com>
2017-07-12 10:20:21 +0100

    facebook login functionality

---
+++ pharosArt/Models/FacebookObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using Umbraco.Core.Services;
+
+namespace pharosArt.Models
+{
+    [DataContract]
+    public class FacebookObject
+    {
+        [DataMember(Name = "birthday")]
+        public string Birthday { get; set; }
+        [DataMember(Name = "gender")]
+        public string Gender { get; set; }
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+        [DataMember(Name = "email")]
+        public string Email { get; set; }
+    }
+}
