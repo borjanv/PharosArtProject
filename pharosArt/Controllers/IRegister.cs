e35b7b011b5f36c959c0043f034f55e764f99409
borjanv <vasovski@me.com>
2017-07-13 14:29:56 +0100

    Registration changes commited

---
+++ pharosArt/Controllers/IRegister.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Security;
+using pharosArt.Models;
+using Umbraco.Core.Models;
+
+namespace pharosArt.Controllers
+{
+    public interface IRegister
+    {
+        MembershipCreateStatus RegisterMember(IUmbracoMember member);
+        int CreateParentMediaFolderForMember(IUmbracoMember member);
+    }
+
+    public interface IUmbracoMember
+    {
+        int Id { get; set; }
+        string Genre { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Password { get; set; }
+        int MediaFolder { get; set; }
+        string Email { get; set; }
+    }
+}
