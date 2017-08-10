a1b2316b03b4693eccdcfa7f33ed4fb4becbff07
Indfear <indfear@gmail.com>
2017-08-10 10:39:53 +0100

    Pagination - Michael

---
+++ pharosArt/Controllers/PaginationController.cs
@@ -0,0 +1,22 @@
+﻿using pharosArt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace pharosArt.Controllers
+{
+    public class PaginationController : SurfaceController
+    {
+        // GET: Pagination
+        public ActionResult ShowPages(int _totalPages, int _currentPage)
+        {
+            var model = new PaginationModel();
+            model.totalPages = _totalPages;
+            model.currentPage = _currentPage;
+            return PartialView("~/Views/Partials/PaginationPartial.cshtml", model);
+        }
+    }
+}
