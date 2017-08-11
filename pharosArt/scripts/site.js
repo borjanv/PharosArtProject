3de12247090f42062b68363fdec02ec3f92a57ee
borjan <vasovski@me.com>
2017-08-11 11:45:54 +0100

    commit from impax

--- pharosArt/scripts/site.js
+++ pharosArt/scripts/site.js
@@ -22,3 +22,11 @@
         }
     });
 }
+
+function hoverFix() {
+    var el = this;
+    var par = el.parentNode;
+    var next = el.nextSibling;
+    par.removeChild(el);
+    setTimeout(function () { par.insertBefore(el, next); }, 0)
+}
